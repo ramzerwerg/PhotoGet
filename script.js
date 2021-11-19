f9d9f49d46a6788b14a5aeab231fc6c5ca389d08
ramzerwerg <43013427+ramzerwerg@users.noreply.github.com>
2021-11-19 19:47:22 +0300

    feature/1/random-img

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const API_KEY = '20199984-e2913538a0f45568cdb4e2fb8';
-let URL, q;
 
 let adress = document.querySelector(".adress");
 
@@ -11,21 +10,24 @@ function setEventListener(){
     document.querySelector(".random-img .fa-arrow-left").addEventListener("click", chooseMethod);
     document.querySelector(".filter-img .fa-arrow-left").addEventListener("click", chooseMethod)
     document.querySelector(".random-img .btn").addEventListener("click", randomImage);
+    document.querySelector(".search_filters").addEventListener("click", openFilters)
 }
 
 // Выбор рандомного изображения в режиме "СЛУЧАЙНАЯ КАРТИНКА"
 function randomImage () {
-    URL = "https://pixabay.com/api/?key="+API_KEY;
+    let URL = "https://pixabay.com/api/?key=" + API_KEY + "&safesearch=true";
     fetch(`${URL}`)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        console.log(data);
         data = data.hits;
         let imgNum = randomInteger(1,20),
         imgURL = data[imgNum].largeImageURL;
 
+        document.querySelector(".random-img .btn__functional").style= "";
+        document.querySelector(".random-img .btn__functional .functional__download").href = `${imgURL}`;
+        document.querySelector(".random-img .btn__functional .functional__show").href = `${imgURL}`;
         document.querySelector(".wrapper").style= `background-image: url(${imgURL});`;
     });
 
@@ -37,9 +39,52 @@ function randomInteger(min, max) {
     return Math.floor(rand);
 }
 
-// TODO: Выбор изображения в режиме "ПОИСК ПО ФИЛЬТРАМ"
+// Выбор изображения в режиме "ПОИСК ПО ФИЛЬТРАМ"
 function filterImage(){
+    let URL = "https://pixabay.com/api/?key=" + API_KEY + "&safesearch=true";
 
+    
+    if(document.querySelector(".categories_list .category.selected")){
+        let category = document.querySelector(".categories_list .category.selected").getAttribute("data-category");
+        URL += "&category=" + category;
+    }
+
+    if(document.querySelector(".search_input").value) {
+        let q = document.querySelector(".search_input").value;
+        URL += "&q=" + q;
+    }
+
+    //поиск изображения
+    fetch(`${URL}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        data = data.hits;
+        let imgCount = document.querySelector(".filters_menu .count_img input").value;
+        
+        document.querySelector(".container_card").innerHTML = "";
+        for(let i = 0; i < imgCount; i++){
+            let imgURL = data[i].largeImageURL;
+            
+            let card = document.createElement('div');
+
+            card.classList.add("card");
+            
+            card.setAttribute("onmouseover", "cardFunc(this, 1)");
+            card.setAttribute("onmouseleave", "cardFunc(this, 0)");
+            
+            card.style = `background: url('${imgURL}'); background-size: 100%;`;
+
+            card.insertAdjacentHTML('beforeEnd', `
+            <div class="card_functions">
+                <a href="${imgURL}" class="functional__download" download>Скачать</a>
+                <a href="${imgURL}" class="functional__show" target="_blank">Посмотреть</a>
+            </div>`);
+
+            document.querySelector(".container_card").append(card);
+        }
+    });
 }
 
 // Переход между методами
@@ -97,4 +142,40 @@ function chooseMethod(e) {
             },100);
          }, 800);
     }
+}
+
+function cardFunc(card, mode) {
+    if(mode == 1){
+        card.querySelector('.card_functions').style="display: flex";
+    } else {
+        card.querySelector('.card_functions').style="display: none";
+    }
+}
+
+function openFilters(){
+    let filtersMenu = document.querySelector(".filters_menu"),
+    searchCaret = document.querySelector(".search_filters i");
+
+    if(filtersMenu.getAttribute("data-closed") == "true"){
+        filtersMenu.style.height =  "185px";
+        filtersMenu.style.padding = "10px";
+        filtersMenu.setAttribute("data-closed", "false");
+        searchCaret.classList.toggle("fa-caret-down");
+        searchCaret.classList.toggle("fa-caret-up");
+    } else {
+        filtersMenu.style.height =  "0px";
+        filtersMenu.style.padding = "0px";
+        filtersMenu.setAttribute("data-closed", "true");
+        searchCaret.classList.toggle("fa-caret-down");
+        searchCaret.classList.toggle("fa-caret-up");
+    }
+}
+
+document.querySelectorAll(".categories_list .category").forEach(item => {
+    item.addEventListener("click", selectCategory);
+});
+
+function selectCategory(e) {
+    document.querySelectorAll(".category").forEach(cat => {cat.classList.remove("selected")})
+    e.target.classList.toggle("selected");
 }
